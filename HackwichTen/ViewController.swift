6e64cd002d5b40b8b082b104e9138f531473b0d7
Marijoy Jatico <142898185+maripandari@users.noreply.github.com>
2023-11-09 12:54:13 -1000

    Completed Part 3

--- HackwichTen/ViewController.swift
+++ HackwichTen/ViewController.swift
@@ -12,11 +12,28 @@ import CoreLocation
 class ViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
+    let initialLocation = CLLocation(latitude: 21.361888, longitude:
+         -158.056022)
+    
+    let regionRadius = 10000
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        centerMapOnLocation(location: initialLocation)
     }
+    
+    func centerMapOnLocation(location:CLLocation)
+           {
+               let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
 
+               latitudinalMeters: CLLocationDistance(regionRadius),
+               longitudinalMeters: CLLocationDistance(regionRadius))
+               mapView.setRegion(coordinateRegion, animated: true)
+
+           }
 
 }
 
