f9e7c51e57cb4491bbaed40ba844eddb6578e096
Marijoy Jatico <142898185+maripandari@users.noreply.github.com>
2023-11-09 12:40:21 -1000

    Completed Parts 1-2

--- HackwichTen/ViewController.swift
+++ HackwichTen/ViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
